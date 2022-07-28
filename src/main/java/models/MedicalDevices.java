6e4b2955b77356894d5b56b78087a14e5cce5858
Nick Choi <96934316+nickchoi10@users.noreply.github.com>
2022-07-28 08:28:49 -0400

    added fields to sneakers and med dev

--- src/main/java/models/MedicalDevices.java
+++ src/main/java/models/MedicalDevices.java
@@ -1,4 +1,11 @@
 package models;
 
 public class MedicalDevices {
+    private String name;
+    private String company;
+    private String typeOfSurgery;
+    private int id;
+    private int qty;
+    private int price;
+
 }
